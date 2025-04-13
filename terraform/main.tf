09f2da9063939355592946ce7cf62fbd687449c2
mohamed-55-iti <mmmnnn11755@gmail.com>
2025-04-14 01:48:27 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,16 +4,15 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
 
-  tags = {
-    Name = "example-instance"
-  }
 
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > ../ansible/hosts"
   }
+}
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI ID
+  instance_type = "t2.micro"
 }
